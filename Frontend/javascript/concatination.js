1ff0b4867b5f1b1d81740d210ac31e58841c32fa
sahil agrawal <agrawalsahil114@gmail.com>
2025-05-30 18:57:55 +0545

    concadination

--- Frontend/javascript/concatination.js
+++ Frontend/javascript/concatination.js
@@ -3,3 +3,15 @@ let time=2
 let rate=4
 let simpleInterest=(principal*time*rate)/100
 console.log("The simple interest is:-"+simpleInterest)
+
+// Areas
+let length=20
+let breadth=10
+let radius=8
+const Pi=3.1415
+area=length*breadth
+area1=length*length
+area2=Pi*radius*radius
+console.log("The area of sqyare is:-"+area)
+console.log("The area of square is:-"+area1)
+console.log("The area of circle is:-"+area2)
