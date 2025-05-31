19ff6db2173cb4a531306ace05a19321fdd6220d
sahil agrawal <agrawalsahil114@gmail.com>
2025-05-31 09:55:45 +0545

    td

--- Frontend/javascript/task/task.js
+++ Frontend/javascript/task/task.js
@@ -72,3 +72,8 @@ console.log("The mean of the given data is: " + mean
 // let h=15
 // area=(1/2)*b*h
 // console.log("The area of triangle is:-"+area)
+
+let b=20
+let h=15
+area=(1/2)*b*h
+console.log("The area of triangle is:-"+area)
