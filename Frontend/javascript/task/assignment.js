0499579a040929968ad842eb0678523af9ef62c1
sahil agrawal <agrawalsahil114@gmail.com>
2025-06-01 19:04:57 +0545

    java

---
+++ Frontend/javascript/task/assignment.js
@@ -0,0 +1,32 @@
+// Assignment operators in java script are used to assign values to any variables and combine assignment with other operations
+// Some assignment operators of java script are:=,+=,-=,*=,/=,%=
+
+// all sign
+let sum=50
+let sub=60
+let product=70
+let division=80
+let modules=90
+sum+=50
+sub-=50
+product*=4
+division/=20
+modules%=46
+console.log("+ sign used "+sum)
+console.log("- sig used "+sub)
+console.log("* sign used "+product)
+console.log("/ sign used "+division)
+console.log("% sign used "+modules)
+
+// multiply
+let number=5
+console.log("5X1="+number*1)
+console.log("5X2="+number*2)
+console.log("5X3="+number*3)
+console.log("5X4="+number*4)
+console.log("5X5="+number*5)
+console.log("5X6="+number*6)
+console.log("5X7="+number*7)
+console.log("5X8="+number*8)
+console.log("5X9="+number*9)
+console.log("5X10="+number*10)
