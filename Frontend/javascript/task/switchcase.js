a27411eb9e3e18d9d8e04ea71f8301004a141cbd
sahil agrawal <agrawalsahil114@gmail.com>
2025-06-09 18:16:36 +0545

    task

---
+++ Frontend/javascript/task/switchcase.js
@@ -0,0 +1,26 @@
+userInput = Number(prompt("Enter any number between 1 to 7 : "))
+switch (userInput){
+    case 1:
+        console.log("Today is Sunday!")
+        break
+    case 2:
+        console.log("Today is Monday!")
+        break
+    case 3:
+        console.log("Today is Tuesday!")
+        break
+    case 4:
+        console.log("Today is Wednesday!")
+        break
+    case 5:
+        console.log("Today is THursday!")
+        break
+    case 6:
+        console.log("Today is Friday!")
+        break
+    case 7:
+        console.log("Today is Saturday!")
+        break
+    default:
+        console.log("Enter correct number.")
+}
