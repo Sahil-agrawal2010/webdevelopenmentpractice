7c14d931a17172852a8396c356d486a414c043c4
sahil agrawal <agrawalsahil114@gmail.com>
2025-06-15 19:00:21 +0545

    While Loop

--- Frontend/javascript/whileLoop.js
+++ Frontend/javascript/whileLoop.js
@@ -1,76 +1,76 @@
-let i=1
-while(i<=10){
-    console.log(i)
-    i++
-}
+// let i=1
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
 
-let i=2
-while(i<=20){
-    console.log(i)
-    i=i+2
-}
+// let i=2
+// while(i<=20){
+//     console.log(i)
+//     i=i+2
+// }
 
-let i=1
-while(i<=50){
-    console.log(i)
-    i=i+2
-}
+// let i=1
+// while(i<=50){
+//     console.log(i)
+//     i=i+2
+// }
 
-let i=10
-while(i>=1){
-    console.log(i)
-    i=i-1
-}
+// let i=10
+// while(i>=1){
+//     console.log(i)
+//     i=i-1
+// }
 
-let i=1
-while(i<=10){
-    console.log(i*i)
-    i=i+1
-}
+// let i=1
+// while(i<=10){
+//     console.log(i*i)
+//     i=i+1
+// }
 
-let num=5
-let i=1
-while(i<=10){
-    console.log("5"+"X"+i+"="+num*i)
-    i=i+1
-}
+// let num=5
+// let i=1
+// while(i<=10){
+//     console.log("5"+"X"+i+"="+num*i)
+//     i=i+1
+// }
 
-let i = 1
-let sum = 0
-while(i<=100){
-    sum = sum + i
-    i++
-}
-console.log(sum)
+// let i = 1
+// let sum = 0
+// while(i<=100){
+//     sum = sum + i
+//     i++
+// }
+// console.log(sum)
 
-let i=50
-while(i>=1){
-    if(i%4==0){
-        console.log(i)
-    }
-        i=i-1
-}
+// let i=50
+// while(i>=1){
+//     if(i%4==0){
+//         console.log(i)
+//     }
+//         i=i-1
+// }
 
-let i=1
-let data = 1
-while(i<=5){
-    console.log(data)
-    data=data*10+1
-    i++
-}
+// let i=1
+// let data = 1
+// while(i<=5){
+//     console.log(data)
+//     data=data*10+1
+//     i++
+// }
 
-let i=2
-let s=1
-while(i<=6){
-    console.log(s)
-    s=s*10+i
-    i=i+1
-}
+// let i=2
+// let s=1
+// while(i<=6){
+//     console.log(s)
+//     s=s*10+i
+//     i=i+1
+// }
 
-let i=1
-let data=1
-while(i<=7){
-    console.log(data)
-    data=data*2
-    i++
-}
+// let i=1
+// let data=1
+// while(i<=7){
+//     console.log(data)
+//     data=data*2
+//     i++
+// }
