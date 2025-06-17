c60087f646e64bb368e469501394833b94ff7d69
sahil agrawal <agrawalsahil114@gmail.com>
2025-06-17 19:03:02 +0545

    1,11,111,1111,11111,111111,11111111,111111111,111111111 in a coloumn

--- Frontend/javascript/nestedLoop.js
+++ Frontend/javascript/nestedLoop.js
@@ -9,3 +9,11 @@ for (let i=1;i<=4;i=i+2){
         console.log("The value of i is "+i+" The value of j is "+j)
     }
 }
+
+for(let i=1;i<=9;i++){
+    let data=""
+    for(let j=1;j<=i;j=j+1){
+    data=data+"1"
+    }
+    console.log(data)
+}
