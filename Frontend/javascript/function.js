4eac66aa6109bdd5535a9fe5eebc224afa136dd2
sahil agrawal <agrawalsahil114@gmail.com>
2025-06-18 18:48:15 +0545

    cube and square

--- Frontend/javascript/function.js
+++ Frontend/javascript/function.js
@@ -13,14 +13,16 @@ function product(a){
 let a=Number(prompt("Enter the Number to find the multiplication table :-"))
 product(a)
 
-
-function product(a){
-    for(let i=1;i<=10;i++){
-     s=a*i
-     console.log(a+"X"+i+"="+s)
-    }
+function square(a){
+    sq=a*a
+    console.log(sq)
+}
+function cube(a){
+    cube_=a*a*a
+    return cube_
 }
 
-
-let a=Number(prompt("Enter the Number to find the multiplication table :-"))
-product(a)
+let a=Number(prompt("Enter any number to see square and cube :- "))
+square(a)
+// let cube_result = cube(a)
+console.log(cube(a))
