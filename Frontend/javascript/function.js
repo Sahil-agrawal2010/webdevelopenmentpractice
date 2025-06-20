62723fcecbe45eeba2e6890da9105a6e1973adf1
sahil agrawal <agrawalsahil114@gmail.com>
2025-06-20 18:38:20 +0545

    to find prime number

--- Frontend/javascript/function.js
+++ Frontend/javascript/function.js
@@ -1,127 +1,144 @@
-function add(a,b){
-    let sum = a+b
-    console.log(sum)
-}
-add(10, 20)
+// function add(a,b){
+//     let sum = a+b
+//     console.log(sum)
+// }
+// add(10, 20)
 
-function product(a){
-    for(let i=1;i<=10;i++){
-     s=a*i
-     console.log(a+"X"+i+"="+s)
-    }
-}
-let a=Number(prompt("Enter the Number to find the multiplication table :-"))
-product(a)
+// function product(a){
+//     for(let i=1;i<=10;i++){
+//      s=a*i
+//      console.log(a+"X"+i+"="+s)
+//     }
+// }
+// let a=Number(prompt("Enter the Number to find the multiplication table :-"))
+// product(a)
 
-function square(a){
-    sq=a*a
-    console.log(sq)
-}
-function cube(a){
-    cube_=a*a*a
-    return cube_
-}
+// function square(a){
+//     sq=a*a
+//     console.log(sq)
+// }
+// function cube(a){
+//     cube_=a*a*a
+//     return cube_
+// }
 
-let a=Number(prompt("Enter any number to see square and cube :- "))
-square(a)
-// let cube_result = cube(a)
-console.log(cube(a))
-
-
-function add(a,b){
-    ad=a+b
-    return ad
-}
-function difference(a,b){
-    diff=a-b
-    return diff
-}
-function product(a,b){
-    pro=a*b
-    return pro
-}
-function divide(a,b){
-    div=a/b
-    return div
-}
-let a=Number(prompt("Enter the first number :- "))
-let b=Number(prompt("Enter the second number :- "))
-console.log()
-console.log(`The sum of ${a} and ${b} is ${add(a,b)}.`)
-console.log(`The difference of ${a} and ${b} is ${difference(a,b)}.`)
-console.log(`The product of ${a} and ${b} is ${product(a,b)}.`)
-console.log(`The division of ${a} and ${b} is ${divide(a,b)}.`)
-// console.log("The sum of "+a+ " and " +b+ " is " +add(a,b)+ ".")
-// console.log("The difference of " +a+ " and " +b+ " is " +difference(a,b)+ ".")
-// console.log("The product of " +a+ " and " +b+ " is " +product(a,b)+".")
-// console.log("The division of " +a+ " and " +b+ " is " +divide(a,b).toFixed(2)+".")
-
-function grade(a){
-    if(a>=90 && a<=100){
-        console.log(`The grade you got by ${a}% is A+`)
-    }
-    else if(a>=80 && a<=90){
-        console.log(`The grade you got by ${a}% is A`)
-    }
-        else if(a>=70 && a<=80){
-        console.log(`The grade you got by ${a}% is B+`)
-    }
-        else if(a>=60 && a<=70){
-        console.log(`The grade you got by ${a}% is B`)
-    }
-        else if(a>=50 && a<=60){
-        console.log(`The grade you got by ${a}% is C+`)
-    }
-    else if(a>=40 && a<=50){
-          console.log(`The grade you got by ${a}% is C`)
-    }
-    else if(a>=0 && a<=40){
-    console.log("You are fail.")
-}
-else{
-    console.log("Enter the valid marks!")
-}
-}
-let a = Number(prompt("Enter the percentage of marks in your exam: "))
-grade(a)
+// let a=Number(prompt("Enter any number to see square and cube :- "))
+// square(a)
+// // let cube_result = cube(a)
+// console.log(cube(a))
 
 
+// function add(a,b){
+//     ad=a+b
+//     return ad
+// }
+// function difference(a,b){
+//     diff=a-b
+//     return diff
+// }
+// function product(a,b){
+//     pro=a*b
+//     return pro
+// }
+// function divide(a,b){
+//     div=a/b
+//     return div
+// }
+// let a=Number(prompt("Enter the first number :- "))
+// let b=Number(prompt("Enter the second number :- "))
+// console.log()
+// console.log(`The sum of ${a} and ${b} is ${add(a,b)}.`)
+// console.log(`The difference of ${a} and ${b} is ${difference(a,b)}.`)
+// console.log(`The product of ${a} and ${b} is ${product(a,b)}.`)
+// console.log(`The division of ${a} and ${b} is ${divide(a,b)}.`)
+// // console.log("The sum of "+a+ " and " +b+ " is " +add(a,b)+ ".")
+// // console.log("The difference of " +a+ " and " +b+ " is " +difference(a,b)+ ".")
+// // console.log("The product of " +a+ " and " +b+ " is " +product(a,b)+".")
+// // console.log("The division of " +a+ " and " +b+ " is " +divide(a,b).toFixed(2)+".")
 
-function simpleInterest(a,b,c){
-    Si=(a*b*c)/100
-    return Si
-}
-let a = Number(prompt("Enter the Principle  :- "))
-let b = Number(prompt("Enter the Time :- "))
-let c = Number(prompt("Enter the Rate :- "))
-console.log()
-console.log(`The simple interest is ${simpleInterest(a,b,c)}`)
+// function grade(a){
+//     if(a>=90 && a<=100){
+//         console.log(`The grade you got by ${a}% is A+`)
+//     }
+//     else if(a>=80 && a<=90){
+//         console.log(`The grade you got by ${a}% is A`)
+//     }
+//         else if(a>=70 && a<=80){
+//         console.log(`The grade you got by ${a}% is B+`)
+//     }
+//         else if(a>=60 && a<=70){
+//         console.log(`The grade you got by ${a}% is B`)
+//     }
+//         else if(a>=50 && a<=60){
+//         console.log(`The grade you got by ${a}% is C+`)
+//     }
+//     else if(a>=40 && a<=50){
+//           console.log(`The grade you got by ${a}% is C`)
+//     }
+//     else if(a>=0 && a<=40){
+//     console.log("You are fail.")
+// }
+// else{
+//     console.log("Enter the valid marks!")
+// }
+// }
+// let a = Number(prompt("Enter the percentage of marks in your exam: "))
+// grade(a)
 
 
-function oddEvenChecker(a){
-    if(a%2){
-        return "Even Number"
-    }
-    else{
-        return "Odd Number"
-    }
-}
 
-let a =Number(prompt("Enter any Number :- "))
-console.log(`${oddEvenChecker(a)}`)
+// function simpleInterest(a,b,c){
+//     Si=(a*b*c)/100
+//     return Si
+// }
+// let a = Number(prompt("Enter the Principle  :- "))
+// let b = Number(prompt("Enter the Time :- "))
+// let c = Number(prompt("Enter the Rate :- "))
+// console.log()
+// console.log(`The simple interest is ${simpleInterest(a,b,c)}`)
 
-let peri=function perimeter(a){
-    const pi=3.1415
-    let r=a/2
-    let circumference=2*pi*r
-    return circumference
-}
-let ar=function area(a){
-    const pi=3.1415
-    let r=a/2
-    let total=pi*r*r
-    return total
-}
-let a=Number(prompt("Enter the diameter :- "))
-console.log(`The circumference of the circle is ${peri(a)}`)
-    console.log(`The area of the cirle is ${ar(a)}`)
+
+// function oddEvenChecker(a){
+//     if(a%2){
+//         return "Even Number"
+//     }
+//     else{
+//         return "Odd Number"
+//     }
+// }
+
+// let a =Number(prompt("Enter any Number :- "))
+// console.log(`${oddEvenChecker(a)}`)
+
+// let peri=function perimeter(a){
+//     const pi=3.1415
+//     let r=a/2
+//     let circumference=2*pi*r
+//     return circumference
+// }
+// let ar=function area(a){
+//     const pi=3.1415
+//     let r=a/2
+//     let total=pi*r*r
+//     return total
+// }
+// let a=Number(prompt("Enter the diameter :- "))
+// console.log(`The circumference of the circle is ${peri(a)}`)
+//     console.log(`The area of the cirle is ${ar(a)}`)
+
+//    let Cheaker=function primnumberChecker(a){
+//        let counter=0
+//   for(let i=1;i<=a;i=i+1){
+//      if(a%i==0){
+//       counter++
+//      }
+//   }
+//   if(counter==2){
+//       console.log(`${a} is a prime number!`)
+//   }
+//   else{
+//       console.log(`${a} is not a prime number!`)
+//   }
+// }
+// let a = Number(prompt("Enter any number to check weather it is a prime number or not :-"))
+//  Cheaker(a)
