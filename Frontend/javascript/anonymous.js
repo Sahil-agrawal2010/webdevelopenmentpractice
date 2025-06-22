219441d3b45e745d4f7f222b99aaf69d01cc164e
sahil agrawal <agrawalsahil114@gmail.com>
2025-06-22 18:33:10 +0545

    multiply

--- Frontend/javascript/anonymous.js
+++ Frontend/javascript/anonymous.js
@@ -7,3 +7,11 @@ function nextFunction(num1, num2){
 }
 
 nextFunction(myFunc(2,3), 10)
+
+let a=Number(prompt("Enter first number :-"))
+let b=Number(prompt("Enter second number :-"))
+let myFunc=function(a,b){
+    return a*b
+}
+myFunc(a,b)
+console.log(`The product is ${myFunc(a,b)}`)
