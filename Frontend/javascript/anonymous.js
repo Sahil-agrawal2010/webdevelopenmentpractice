2080369d446d8dbe0ca7caec423c4b5a2be4a4fe
sahil agrawal <agrawalsahil114@gmail.com>
2025-06-22 18:53:58 +0545

    to find weather the result is positive or not

--- Frontend/javascript/anonymous.js
+++ Frontend/javascript/anonymous.js
@@ -15,3 +15,22 @@ let myFunc=function(a,b){
 }
 myFunc(a,b)
 console.log(`The product is ${myFunc(a,b)}`)
+
+let myFunc=function(a,b){
+    return a-b
+}
+
+function nextfunction(value){
+    if(value<0){
+        console.log("The result is negative!")
+    }
+    else if(value>0){
+        console.log("The result is positive!")
+    }
+    else{
+        console.log("The result is zero")
+    }
+}
+let a=Number(prompt("Enter the first number :-"))
+let b=Number(prompt("Enter the second number :-"))
+nextfunction(myFunc(a,b))
