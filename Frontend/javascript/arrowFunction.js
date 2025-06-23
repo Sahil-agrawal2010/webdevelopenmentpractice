cead9a32ac20efd52618690f6fe5a53612f52c0b
sahil agrawal <agrawalsahil114@gmail.com>
2025-06-23 19:10:14 +0545

    tasks

---
+++ Frontend/javascript/arrowFunction.js
@@ -0,0 +1,25 @@
+let add = (a,b) => {
+    console.log(a+b)
+}
+add(5,2)
+
+let greet=(name)=>{
+    console.log(`Good Morning!`+name)
+}
+name=prompt("Enter the name :- ")
+greet(name)
+
+let arrow=(a,b) =>{
+    console.log(`The area of rectanglr is ${a*b}`)
+}
+let a=prompt("Enter the length to calculate its area :- ")
+let b=prompt("Enter the breadth to calculate its area :- ")
+arrow(a,b)
+
+// Write a programme to calculate area of circle
+// Write a programme to calcualte acceelration
+// Write a programme if the number id divisible by both 5 and 11
+// Write a programme to calculate distance,return speed if speed is 30 or less
+// write a programme to calcualte volume of a cuboid
+// Write a programme to calculate from celcious to faranite
+// Write a programme to calculate from faranite to celcious
