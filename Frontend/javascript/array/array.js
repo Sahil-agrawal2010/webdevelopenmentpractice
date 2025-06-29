82b7c09fafdb949667da28ca1fc6ff63e36d9b80
sahil agrawal <agrawalsahil114@gmail.com>
2025-06-29 19:11:29 +0545

    array

---
+++ Frontend/javascript/array/array.js
@@ -0,0 +1,7 @@
+array is the collection of multiple datas.
+
+let fruits = ["Apple", "Mango", "Pineapple"]
+console.log(fruits)
+
+let fruits = new Array ("Apple", "Mango", "Pineapple")
+console.log(fruits[0])
