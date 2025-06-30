d87b8635920ba4a3b110e5bd9c999a6c1df122dc
sahil agrawal <agrawalsahil114@gmail.com>
2025-06-30 09:58:16 +0545

    learnt few about object and calss

--- Frontend/javascript/array/array.js
+++ Frontend/javascript/array/array.js
@@ -5,3 +5,17 @@ console.log(fruits)
 
 let fruits = new Array ("Apple", "Mango", "Pineapple")
 console.log(fruits[0])
+
+away from the topic
+class shose{
+    constructor(brand,size,colour){
+        this.brand=brand
+        this.size=size
+        this.colour=colour
+    }
+    shosedetails(){
+        console.log(`${this.brand},${this.size},${this.colour}`)
+    }
+}
+const myShose=new shose("Nike",40,"Green")
+myShose.shosedetails()
