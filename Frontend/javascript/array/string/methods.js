71b27bec7c47b2a00a779edea735a84167a74d47
sahil agrawal <agrawalsahil114@gmail.com>
2025-07-15 19:01:52 +0545

    learned different methods on string

---
+++ Frontend/javascript/array/string/methods.js
@@ -0,0 +1,24 @@
+let name="My name is Sahil"
+let data="I live in Birtamode Jhapa"
+console.log(name.length)
+console.log(name.charAt(1))
+console.log(name.at(1))
+console.log(name[1])
+let data1=name .concat(" " ,data)
+console.log(data1)
+let fruits="apple,banana,kiwi"
+let data2=fruits.slice(6,12)
+console.log(data2)
+let data3=fruits.slice(6)
+console.log(data3)
+let data4=fruits.slice(-11)
+console.log(data4)
+let data5=fruits.slice(-11,-5)
+console.log(data5)
+let firstName="sahil"
+let secondName="AGRAWAL"
+console.log(firstName.toUpperCase())
+console.log(secondName.toLowerCase())
+let name2="     Sahil   "
+console.log(name2)
+console.log(name2.trim())
