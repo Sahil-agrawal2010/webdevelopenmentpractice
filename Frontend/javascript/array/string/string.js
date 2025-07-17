55c0bf37304bea95228512a2efd7e2afd7f63293
sahil agrawal <agrawalsahil114@gmail.com>
2025-07-17 18:15:03 +0545

    ends with method done in string

--- Frontend/javascript/array/string/string.js
+++ Frontend/javascript/array/string/string.js
@@ -16,19 +16,34 @@ console.log(alphabet.length)
 let name2="Hey my name is \"Sahil\""
 console.log(name2)
 
+
 let data=`It\'\s Sahil here`
 console.log(data)
+
 console.log("I am first line\nI am second line")
 console.log("I am Nayan\tI am getting 4 space. ")
+
+
 let num1 = 4
 let num2 =  "4"
 console.log(num1===num2)
+
+
 let text="Hi I am Sahil"
 let variable=text.replace("I","My")
 console.log(variable)
-let text="Hi I am Sahil"
+
+
+let textx="Hi I am Sahil"
 let variable=text.split("")
 console.log(variable)
+
+
 let text="Hi I am Sahil"
 let variable=text.startsWith("H")
+console.log(variable)
+
+
+let text="Hi I am Sahil"
+let variable=text.endsWith("l")
 console.log(variable)
