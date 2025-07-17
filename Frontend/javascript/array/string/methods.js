442b33429637760a733fce86dc295e1b5039966e
sahil agrawal <agrawalsahil114@gmail.com>
2025-07-17 18:17:57 +0545

    ....

--- Frontend/javascript/array/string/methods.js
+++ Frontend/javascript/array/string/methods.js
@@ -4,30 +4,47 @@ console.log(name.length)
 console.log(name.charAt(1))
 console.log(name.at(1))
 console.log(name[1])
+
+
 let data1=name .concat(" " ,data)
 console.log(data1)
+
+
 let fruits="apple,banana,kiwi"
 let data2=fruits.slice(6,12)
 console.log(data2)
+
+
 let data3=fruits.slice(6)
 console.log(data3)
+
+
 let data4=fruits.slice(-11)
 console.log(data4)
+
+
 let data5=fruits.slice(-11,-5)
 console.log(data5)
+
+
 let firstName="sahil"
 let secondName="AGRAWAL"
 console.log(firstName.toUpperCase())
 console.log(secondName.toLowerCase())
+
+
 let name2="     Sahil   "
 console.log(name2)
 console.log(name2.trim())
+
+
 let text="5"
 let ps=text.padStart(4,"O")
 let pe=text.padEnd(4,"X")
 console.log(text)
 console.log(ps)
-console.log(pe)  
+console.log(pe) 
+
 
 let a=1
 let b=4
--- Frontend/javascript/array/string/string.js
+++ Frontend/javascript/array/string/string.js
@@ -1,49 +1,49 @@
-let name="It's me Sahil Agrawal"
-console.log(name)
+// let name="It's me Sahil Agrawal"
+// console.log(name)
 
 
-let school=`Green field world school`
-console.log(school)
+// let school=`Green field world school`
+// console.log(school)
 
 
-let name1=`That is "Lion"`
-console.log(name1)
+// let name1=`That is "Lion"`
+// console.log(name1)
 
-let alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-console.log(alphabet.length)
-
-
-let name2="Hey my name is \"Sahil\""
-console.log(name2)
+// let alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// console.log(alphabet.length)
 
 
-let data=`It\'\s Sahil here`
-console.log(data)
-
-console.log("I am first line\nI am second line")
-console.log("I am Nayan\tI am getting 4 space. ")
+// let name2="Hey my name is \"Sahil\""
+// console.log(name2)
 
 
-let num1 = 4
-let num2 =  "4"
-console.log(num1===num2)
+// let data=`It\'\s Sahil here`
+// console.log(data)
+
+// console.log("I am first line\nI am second line")
+// console.log("I am Nayan\tI am getting 4 space. ")
 
 
-let text="Hi I am Sahil"
-let variable=text.replace("I","My")
-console.log(variable)
+// let num1 = 4
+// let num2 =  "4"
+// console.log(num1===num2)
 
 
-let textx="Hi I am Sahil"
-let variable=text.split("")
-console.log(variable)
+// let text="Hi I am Sahil"
+// let variable=text.replace("I","My")
+// console.log(variable)
 
 
-let text="Hi I am Sahil"
-let variable=text.startsWith("H")
-console.log(variable)
+// let textx="Hi I am Sahil"
+// let variable=text.split("")
+// console.log(variable)
 
 
-let text="Hi I am Sahil"
-let variable=text.endsWith("l")
-console.log(variable)
+// let text="Hi I am Sahil"
+// let variable=text.startsWith("H")
+// console.log(variable)
+
+
+// let text="Hi I am Sahil"
+// let variable=text.endsWith("l")
+// console.log(variable)
