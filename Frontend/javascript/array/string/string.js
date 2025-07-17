f0141cac67b783828cc9bcc65a24d630ba0f0988
sahil agrawal <agrawalsahil114@gmail.com>
2025-07-17 18:12:04 +0545

    split method done in string

--- Frontend/javascript/array/string/string.js
+++ Frontend/javascript/array/string/string.js
@@ -1,13 +1,21 @@
 let name="It's me Sahil Agrawal"
 console.log(name)
+
+
 let school=`Green field world school`
 console.log(school)
+
+
 let name1=`That is "Lion"`
 console.log(name1)
+
 let alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 console.log(alphabet.length)
+
+
 let name2="Hey my name is \"Sahil\""
 console.log(name2)
+
 let data=`It\'\s Sahil here`
 console.log(data)
 console.log("I am first line\nI am second line")
@@ -17,4 +25,7 @@ let num2 =  "4"
 console.log(num1===num2)
 let text="Hi I am Sahil"
 let variable=text.replace("I","My")
+console.log(variable)
+let text="Hi I am Sahil"
+let variable=text.split("")
 console.log(variable)
