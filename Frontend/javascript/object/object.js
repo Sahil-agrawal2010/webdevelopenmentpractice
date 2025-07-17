fdec9cffb264d88a831a459945c9591e80867737
sahil agrawal <agrawalsahil114@gmail.com>
2025-07-17 21:19:32 +0545

    task done

--- Frontend/javascript/object/object.js
+++ Frontend/javascript/object/object.js
@@ -15,3 +15,74 @@ person.eyeColour="Blue"
 person.isMarried=true
 console.log(person.firstName)
 console.log(person["age"])
+
+const bikeDetail={
+    bikeName:"Duggati",
+    colour:"Blue",
+    price:200000,
+    isSold:true
+    cc:220,
+}
+const areoplaneDetail={
+    aeroplaneName:"Indigo",
+    colour:"Blue",
+    price:20000000,
+    isSold:true
+    cc:500,
+}
+const tvDetails={
+    tvName:"Life's Good",
+    colour:"Blue",
+    price:200000,
+    isSold:true
+    cc:"HD",
+}
+const mobileDetails={
+    mobileName:"Xiamoi",
+    colour:"Blue",
+    price:200000,
+    isSold:true
+    cc:"HD",
+}
+const tabDetails={
+    tabName:"Xiamoi",
+    colour:"Blue",
+    price:200000,
+    isSold:true
+    cc:"HD",
+}
+const purifierDetails={
+    purifierName:"RO",
+    colour:"Blue",
+    price:200000,
+    isSold:true
+    cc:"Purified Water",
+}
+const airconditionerDetails={
+    airconditionerDetails:"Life's Good",
+    colour:"Blue",
+    price:200000,
+    isSold:true
+    cc:"Pure Air",
+}
+const calculatorDetails={
+    calculatorName:"Casio",
+    colour:"Blue",
+    price:200000,
+    isSold:true,
+    cc:"Pure Claculation"
+}
+const lightDetails={
+    lightName:"TubeLight",
+    colour:"Blue",
+    price:200000,
+    isSold:true,
+    cc:250
+}
+const speakerDetails={
+    speakerName:"JBL",
+    colour:"Blue",
+    price:200000,
+    isSold:true,
+    cc:450
+}
