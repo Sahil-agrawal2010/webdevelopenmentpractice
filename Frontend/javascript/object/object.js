ff082c7bcb3234496f59e2683970e5c3a7984445
sahil agrawal <agrawalsahil114@gmail.com>
2025-07-17 18:38:47 +0545

    object is being learned

---
+++ Frontend/javascript/object/object.js
@@ -0,0 +1,17 @@
+const carDetails={
+    carName:"BMW",
+    colour:"Blue",
+    price:2000000,
+    isSold:true,
+    cc:1500
+}
+const name=carDetails.carName
+console.log(name)
+const person=new Object()
+person.firstName="Rama"
+person.lastName="Agrawal"
+person.age=44
+person.eyeColour="Blue"
+person.isMarried=true
+console.log(person.firstName)
+console.log(person["age"])
