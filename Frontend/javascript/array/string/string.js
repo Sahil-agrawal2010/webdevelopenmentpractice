3f12f3239e04eeb5d4fc5de7ce6fdbda8990eef5
sahil agrawal <agrawalsahil114@gmail.com>
2025-07-17 18:09:43 +0545

    replace method done in string ok

--- Frontend/javascript/array/string/string.js
+++ Frontend/javascript/array/string/string.js
@@ -15,3 +15,6 @@ console.log("I am Nayan\tI am getting 4 space. ")
 let num1 = 4
 let num2 =  "4"
 console.log(num1===num2)
+let text="Hi I am Sahil"
+let variable=text.replace("I","My")
+console.log(variable)
