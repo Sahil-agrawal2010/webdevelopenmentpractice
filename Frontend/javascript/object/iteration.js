42d1ef4b6e8f2df92649d1e3e07897928b6f7bfb
sahil agrawal <agrawalsahil114@gmail.com>
2025-07-21 19:07:29 +0545

    total number of keys done to find

--- Frontend/javascript/object/iteration.js
+++ Frontend/javascript/object/iteration.js
@@ -22,3 +22,22 @@ const student={
 for(let Keys in student){
     console.log(Keys +"+"+ student[Keys])
 }
+
+
+const student={
+    name:"Sahil Agrawal",
+    classss:10,
+    section:"A",
+    stop:"Onestop",
+    grade:"A+",
+    eyeColour:"Blue",
+    hairColour:"Black",
+    school:"Green Field World School"
+}
+let c = 0
+for(let Keys in student){
+    c++
+}
+console.log(c)
+let totalProperties = Object.keys(student).length;
+console.log(totalProperties);
