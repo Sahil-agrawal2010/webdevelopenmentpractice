6a8a0e52cab8371f5586ff77f799f84efbd57814
sahil agrawal <agrawalsahil114@gmail.com>
2025-07-25 19:05:52 +0545

    math done in java script

---
+++ Frontend/javascript/math/math.js
@@ -0,0 +1,9 @@
+    console.log(Math.PI)
+console.log(Math.E)
+
+let numb1=10
+let numb3=33.378
+console.log(Math.round(numb3))
+console.log(Math.floor(numb3))
+console.log(Math.ceil(numb3))
+console.log(Math.trunc(numb3))
