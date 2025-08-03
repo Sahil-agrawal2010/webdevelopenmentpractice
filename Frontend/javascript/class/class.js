14bdbc320753ea0ea295eb1dbf72bd8c5c84e5c4
sahil agrawal <agrawalsahil114@gmail.com>
2025-08-03 21:15:32 +0545

    task done

--- Frontend/javascript/class/class.js
+++ Frontend/javascript/class/class.js
@@ -80,3 +80,65 @@ let obj1=new login("Dhairya",1234)
 obj1.checker("Dhairya",1234)
 let obj2=new login("Hari",14)
 obj2.checker("Ram",14)
+
+
+Write a programme to creat a fan class with status property on or off.To add methods turn on,turn off and get status
+class fan{
+    constructor(fan1){
+        this.fan1=fan1
+    }
+    checker(){
+        if(this.fan1=="ON"){
+            console.log("The fan is ON!")
+        }
+        else{
+            console.log("The fan is OFF!")
+        }
+    }
+}
+let obj1=new fan("ON")
+obj1.checker()
+let obj2=new fan("OFF")
+obj2.checker()
+
+
+Write a programme to creat a product class with name and price.Adda method apply ten (10) percentade discount if the price is aboue than ten (10) thousand.
+class shopping{
+    constructor(name,total){
+        this.name=name
+        this.total=total
+    }
+    discount(){
+        if(this.total>10000){
+            console.log(`Your total amount after discount is ${(this.total-(10/100)*this.total)}`)
+        }
+        else{
+            console.log("Sorry!You got no discount")
+        }
+    }
+}
+obj1=new shopping("Sahil Agrawal",20000)
+obj1.discount()
+obj2=new shopping("Dhairya Agrawal",2000)
+obj2.discount()
+
+
+Write a programme to creat a passanger class with name and balance.There is a method buy ticket that reduce rs (fifty)50 from main balance if the main balance is less than fifty(50) don't sell.
+class passanger{
+    constructor(name,balance){
+        this.name=name
+        this.balance=balance
+    }
+    function(){
+        if(this.balance>=50){
+            console.log("Add to Cart!")
+        }
+        else{
+            console.log("Sorry!You cant buy ticket!")
+        }
+    }
+}
+obj1=new passanger("Sahil Agrawal",60)
+obj1.function()
+obj2=new passanger("Dhairya Agrawal",40)
+obj2.function()
