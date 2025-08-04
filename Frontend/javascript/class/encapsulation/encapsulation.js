d101ae0435c332099e44da440f2ee4c673fabd86
sahil agrawal <agrawalsahil114@gmail.com>
2025-08-04 18:56:07 +0545

    learned more as in getteer

--- Frontend/javascript/class/encapsulation/encapsulation.js
+++ Frontend/javascript/class/encapsulation/encapsulation.js
@@ -9,3 +9,32 @@ class person{
 }
 const obj1=new person("Sahil Agrawal",15)
 console.log(obj1.agee)
+
+
+class person{
+    constructor(name,age){
+        this.name=name
+        this.age=age
+    }
+    get agee(){
+        return this.age
+    }
+    
+    display(){
+        console.log("My name is : "+this.name+" and my age is : "+this.age)
+    }
+}
+const obj1=new person("Sahil Agrawal",15)
+console.log(obj1.agee) // in getter method we can access any method without using ()
+console.log(obj1.name)
+console.log(obj1.age)
+obj1.display()
+
+
+// in order to take method as a variable then getter is used
+
+// varaibles - attributes, properties, fields
+// methods - functions
+
+
+// class ko object jun baninxa, teslai class ko instance vanincha
