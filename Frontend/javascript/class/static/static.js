6084dbeba1c16fe5aebfc25950368f7777d193bc
sahil agrawal <agrawalsahil114@gmail.com>
2025-08-06 19:09:24 +0545

    counting of object done

--- Frontend/javascript/class/static/static.js
+++ Frontend/javascript/class/static/static.js
@@ -8,3 +8,19 @@ class Calculate{
 }
 console.log(Calculate.value)
 console.log(Calculate.cube(5))
+
+
+class Haha{
+    static count = 0
+    constructor(){
+        Haha.count ++
+    }
+    
+    static getTotal(){
+        console.log("The total number of users is : "+Haha.count)
+    }
+}
+
+let obj1 = new Haha()
+let obj2 = new Haha()
+Haha.getTotal()
