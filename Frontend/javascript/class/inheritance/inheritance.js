41fe5e3a4d8c8500d98107ba39a128b14ae397be
sahil agrawal <agrawalsahil114@gmail.com>
2025-08-18 11:44:45 +0545

    all task done of inheritense

--- Frontend/javascript/class/inheritance/inheritance.js
+++ Frontend/javascript/class/inheritance/inheritance.js
@@ -75,5 +75,71 @@ obj3.sound()
 const obj4=new horse()
 obj4.sound()
 
+
 Write a programme  to creat multilevel inheritence
+class Animal{
+    sound(){
+        console.log("All naimals produce some sound!")
+    }
+}
+class dog extends Animal{
+}
+class cat extends Animal{
+}
+class horse extends Animal{
+}
+class elephant extends Animal{
+}
+class rhino extends Animal{
+}
+class hippopotamus extends Animal{
+}
+class ants extends Animal{
+}
+class bees extends Animal{
+}
+class grassHopper extends Animal{
+}
+class lizard extends Animal{
+}
+class tiger extends Animal{
+}
+const obj=new Animal()
+obj.sound()
+const obj2=new cat()
+obj2.sound()
+
+
 Write a programme to show hierarchial inherientence creat a parent class shape,creat three(3) child class circle,square,rectangle and calculate their area
+class shape{
+    length(){
+        let l=10
+        return l
+    }
+}
+class circle extends shape{
+    area(){
+    let pi=3.1415
+    let area=pi*(this.length()*this.length())
+    console.log(area)
+    }
+}
+class square extends shape{
+    area1(){
+        let area1=this.length()*this.length()
+        console.log(area1)
+    }
+}
+class rectangle extends shape{
+    area2(){
+        let b=5
+        let area2=this.length()*b
+        console.log(area2)
+    }
+}
+obj=new circle()
+obj.area()
+obj1=new square()
+obj1.area1()
+obj2=new rectangle()
+obj2.area2()
