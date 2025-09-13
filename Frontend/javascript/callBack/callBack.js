969110d87080251c708dbd056c31d8944034856a
sahil agrawal <agrawalsahil114@gmail.com>
2025-09-13 18:10:43 +0545

    odd even number checker done

--- Frontend/javascript/callBack/callBack.js
+++ Frontend/javascript/callBack/callBack.js
@@ -23,3 +23,28 @@ let myFunc= function() {
     console.log("I am the function without name.")
 }
 name("12","10",myFunc)
+
+
+function name(age,grade,callback){
+    console.log(`My age is ${age}.I read in grade ${grade}.`)
+    callback()
+}
+name("12","10",function() {
+    console.log("I am the function without name.")
+})   
+// the direct function is placed while writin gthe function output
+
+
+let firsr_function=function(a,callback){
+    callback(a)
+}
+let second_function=function(a){
+    if(a%2===0){
+        console.log("The number is a even number.")
+    }
+    else{
+        console.log("The number you have entered is odd number.")
+    }
+}
+let a=Number(prompt("Enter the number:- "))
+firsr_function(a,second_function)
