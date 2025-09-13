7b230fa64b199e1a896ce16b0453e3426d34745f
sahil agrawal <agrawalsahil114@gmail.com>
2025-09-13 17:49:36 +0545

    anonomonous fuctio done on callback

--- Frontend/javascript/callBack/callBack.js
+++ Frontend/javascript/callBack/callBack.js
@@ -13,3 +13,13 @@ let name = prompt("Enter your name : ")
 let age = prompt("Enter your age : ")
 
 main(name, age, second)
+
+
+function name(age,grade,callback){
+    console.log(`My age is ${age}.I read in grade ${grade}.`)
+    callback()
+}
+let myFunc= function() {
+    console.log("I am the function without name.")
+}
+name("12","10",myFunc)
