c89f39309318849f8a649ca722d594708ea56a29
sahil agrawal <agrawalsahil114@gmail.com>
2025-09-13 18:22:44 +0545

    calculator done

--- Frontend/javascript/callBack/callBack.js
+++ Frontend/javascript/callBack/callBack.js
@@ -48,3 +48,59 @@ let second_function=function(a){
 }
 let a=Number(prompt("Enter the number:- "))
 firsr_function(a,second_function)
+
+
+let first_function=function(a,s,m,d,numb,numb2,o){
+    a(numb,numb2,o)
+    s(numb,numb2,o)
+    m(numb,numb2,o)
+    d(numb,numb2,o)
+}
+let add=function(numb,numb2,o){
+    if(o==="+"){
+        console.log(`The addition is ${numb+numb2}`)
+    }
+}
+let subtraction=function(numb,numb2,o){
+    if(o==="-"){
+        console.log(`The subtraction is ${numb-numb2}`)
+    }
+}
+let multiply=function(numb,numb2,o){
+    if(o==="*"){
+        console.log(`The multiplication is ${numb*numb2}`)
+    }
+}
+let divide=function(numb,numb2,o){
+    if(o==="/"){
+        console.log(`The division is ${numb/numb2}`)
+    }
+}
+let numb=Number(prompt("Enter the first number :- "))
+let numb2=Number(prompt("Enter the second number :- "))
+let o=prompt("Enter the operator:- ")
+first_function(add,subtraction,multiply,divide,numb,numb2,o)
+
+
+let first_function=function(a,s,m,d,numb,numb2,o){
+    a(numb,numb2,o)
+    s(numb,numb2,o)
+    m(numb,numb2,o)
+    d(numb,numb2,o)
+}
+let numb=Number(prompt("Enter the first number :- "))
+let numb2=Number(prompt("Enter the second number :- "))
+let o=prompt("Enter the operator:- ")
+first_function(function(numb,numb2,o){
+    if(o==="+"){
+        console.log(`The addition is ${numb+numb2}`)
+    }},function(numb,numb2,o){
+    if(o==="-"){
+        console.log(`The subtraction is ${numb-numb2}`)
+    }},function(numb,numb2,o){
+    if(o==="*"){
+        console.log(`The multiplication is ${numb*numb2}`)
+    }},function(numb,numb2,o){
+    if(o==="/"){
+        console.log(`The division is ${numb/numb2}`)
+    }},numb,numb2,o)
