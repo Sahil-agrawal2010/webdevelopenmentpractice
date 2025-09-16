9c0c08f34709880b6807d79a53fb16463ead0492
sahil agrawal <agrawalsahil114@gmail.com>
2025-09-16 18:26:44 +0545

    set time out done

---
+++ Frontend/javascript/asynchronous/asynchronous.js
@@ -0,0 +1,10 @@
+    Synchronous is the default in javascript.Our code runs line by line one after the other.If one line is taking time rest waits so this is the Synchronous code behavior in javascript.
+
+    In Asynchronous way our code doesnot wait for slower task like API calls , timers , filereading.
+    It transfers those task and continues with the rest of the code.Once the task is done , it notifies back using cllbacks,promisis or async/await
+
+console.log("I am Very Fast")
+console.log("I am just fast")
+setTimeout(()=>{
+    console.log("I am slow!")
+},5000)
