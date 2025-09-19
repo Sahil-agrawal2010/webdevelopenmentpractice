e84505488da2484b5a4986540fe88fcc1ffa68de
sahil agrawal <agrawalsahil114@gmail.com>
2025-09-19 17:24:43 +0545

    hello + anything done through promises and async used

---
+++ Frontend/javascript/promise/promiseBasic.js
@@ -0,0 +1,31 @@
+let userInput = Number(prompt("Enter yoour age :- "))
+let promisee = new Promise((resolve, reject) => {
+    if (userInput >= 18) {
+        resolve("You are able to vote!")
+    } else {
+        reject("Sorry!You are unable to vote.")
+    }
+})
+
+promisee.then((message) => {
+        console.log(message)
+    }).catch((err)=>{
+        console.log(err)
+    })
+
+
+    let promisee=new Promise((resolve,reject) =>{
+    let userInput1=prompt("Enter the input:- ")
+    if(!userInput1){
+        reject("Sorry, please enter your username.")
+    }else{
+        setTimeout(()=>{
+            resolve("Hello " + userInput1)
+        }, 5000)
+    }
+})
+promisee.then((value)=>{
+    console.log(value)
+}).catch((error)=>{
+    console.log(error)
+})
