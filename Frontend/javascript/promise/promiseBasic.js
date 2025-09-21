b8de351af99be9412731713ae5b0dbc19aba783c
sahil agrawal <agrawalsahil114@gmail.com>
2025-09-21 20:53:29 +0545

    second task done

--- Frontend/javascript/promise/promiseBasic.js
+++ Frontend/javascript/promise/promiseBasic.js
@@ -120,3 +120,24 @@ let promise=new Promise((resolve)=>{
 })
 
 Write a programme to creat a promise that resolves with 10. First .than should return a new promise that doubles the number after two second. Second .than should triple the number after four second . Third .than should find the square , cube and square root of the that number.
+let promise=new Promise((resolve)=>{
+    userInput=10
+    resolve(userInput)
+})
+.then((value)=>{
+    setTimeout(()=>{
+    console.log(userInput*2)
+    },2000)
+    return value
+})
+.then((value)=>{
+    setTimeout(()=>{
+        console.log(userInput*3)
+    },4000)
+    return value
+})
+.then((value)=>{
+    console.log(userInput*userInput)
+        console.log(userInput*userInput*userInput)
+        console.log(Math.sqrt(userInput))
+})
