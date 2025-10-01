d9cbf9660f816f75a13aac246a935ddfcd9e7a51
sahil agrawal <agrawalsahil114@gmail.com>
2025-10-01 23:06:26 +0545

    vant do any thing today

--- Frontend/javascript/promise/promiseBasic.js
+++ Frontend/javascript/promise/promiseBasic.js
@@ -625,3 +625,4 @@ for(let i=0;i<value.length;i++){
 
     console.log("I am a good boy and i havea to do some task and also some projects")
     console.log("Nothing done today also will continue from day after tommorow i am out somewhwere so.")
+    cons0ole.log("Cant do any thong today also oout somewhere in nepal only.")
