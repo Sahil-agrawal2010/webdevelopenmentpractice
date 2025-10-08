bd4218a3b405ad042a135899936e500142044be5
sahil agrawal <agrawalsahil114@gmail.com>
2025-10-08 18:29:58 +0545

    done a bit in Json

--- Frontend/javascript/JSON/Json.js
+++ Frontend/javascript/JSON/Json.js
@@ -1,1 +1,37 @@
 Java Script Object Notation
+// API data - JSON - String format, Object format 
+
+// Object - String - stringify
+// String - object - parse
+
+
+let Json={
+    name:"Sahil",
+    lastName:"Agrwal",
+    grade:10,
+    favouriteNumber:[10,14,18,22],
+    address:{
+        country:"Nepal",
+        city:"Birtamode"
+    }
+}
+console.log(typeof(Json))
+let Json2=JSON.stringify(Json)
+console.log(typeof(Json2))
+console.log(Json2)
+console.log(JSON.parse(Json2))
+
+
+let data = '{"name":"Sahil", "lastName":"Agrwal"}'
+   
+console.log(typeof(data)) 
+console.log(data)
+
+
+
+let newData = JSON.parse(data)
+console.log(typeof(newData))
+console.log(newData)
+
+let secondData = JSON.stringify(newData)
+console.log(typeof(secondData))
