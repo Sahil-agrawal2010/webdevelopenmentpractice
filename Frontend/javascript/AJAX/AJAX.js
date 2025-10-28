3522d775048427c487ec064f1c57f720a28a9e47
sahil agrawal <agrawalsahil114@gmail.com>
2025-10-28 18:33:44 +0545

    learned ajax

--- Frontend/javascript/AJAX/AJAX.js
+++ Frontend/javascript/AJAX/AJAX.js
@@ -1,2 +1,17 @@
 // Asynchronous Javascript And Xml:-It is used to refresh the content of the website without Refereshing.
 // Incomplete Loading....
+
+fetch(`https://68ef93b8b06cc80282966c.mockapi.io/empyee`)
+.then(value=>{
+    return value.json()
+})
+.then(result=>{
+    console.log(result)
+})
+.catch(err=> console.log("testing error"))
+
+
+fetch(`https://68ef93b8b06cc802829df66c.mockapi.io/employee`)
+  .then(value => value.json())
+  .then(result => console.log(result))
+  .catch(err => console.log('Something went wrong:', err));
