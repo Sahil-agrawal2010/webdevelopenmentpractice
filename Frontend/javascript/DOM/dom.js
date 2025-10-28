8fbf8ef22eb43ca4280d106e87ea0bf2598e5fd2
sahil agrawal <agrawalsahil114@gmail.com>
2025-10-28 18:53:43 +0545

    wrote syllabus of dome

---
+++ Frontend/javascript/DOM/dom.js
@@ -0,0 +1,17 @@
+// DOM Syllabus
+// 1)Introduction to DOM
+// 2)Accessing DOM elements
+// 3)Traversing the DOM
+// 4)Manipulating DOM content
+// 5)Creating and removing elements
+// 6)DOM collections and node list
+// 7)Event handeling
+// 8)Forms and input manipulation
+// 9)DOM styleing and CSS manipulation
+// 10)DOM animations and transitions
+// 11)Advanced event concepts
+// 12)DOM measurnments and geometry
+// 13)Working with attributes and data artributes
+// 14)Local storage,session storage and cookies
+// 15)Working with iframes and external content
+// 16)DOM and BOM
