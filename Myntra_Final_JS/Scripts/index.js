7e62fae86d9729c485b03cf5d9b7b9277c45ec8d
sahil agrawal <agrawalsahil114@gmail.com>
2025-12-11 20:58:08 +0545

    myntra project fully done

--- Myntra_Final_JS/Scripts/index.js
+++ Myntra_Final_JS/Scripts/index.js
@@ -1,40 +1,25 @@
-let itemContainer=document.querySelector('.items-container')
-
-let product_Data=[
-    {
-        image:"/Myntra_Final_JS/images/1.jpg",
-        rating:{
-            stars:4.5 ,
-            totalReviews:1400
-        },
-        company_name:"Carlton London",
-        item_name:"Rhodium-Plated CZ Floral Studs",
-        price:{
-            current_price:606,
-            original_price:1045,
-            discount:42
-        }
-
-    }
-]
-
-itemContainer.innerHTML=`
+let output_container=document.querySelector('.items-container')
+let itemContainer=""
+allItems.forEach((item) => {
+itemContainer += `
         <div class="product_div_one">
           <div class="product_image1">
             <img
-              src="${product_Data[0].image}"
+              src="${item.image}"
               alt="EarRings Image"
               height="300px"
             />
           </div>
-          <p class="rating_product_one">${product_Data[0].rating.stars} ⭐ | ${product_Data[0].rating.totalReviews}</p>
-          <p class="name_product_one">${product_Data[0].company_name}</p>
-          <p class="article_product_one">${product_Data[0].item_name}</p>
+          <p class="rating_product_one">${item.rating.stars} ⭐ | ${item.rating.totalReviews}</p>
+          <p class="name_product_one">${item.company_name}</p>
+          <p class="article_product_one">${item.item_name}</p>
           <div class="price_product_div_one">
-            <div class="main_price_one">Rs.${product_Data[0].price.current_price}</div>
-            <div class="marked_price_one"><del>Rs ${product_Data[0].price.original_price}</del></div>
-            <div class="discount_percentage_one">(%${product_Data[0].price.discount} OFF)</div>
+            <div class="main_price_one">Rs.${item.price.current_price}</div>
+            <div class="marked_price_one"><del>Rs ${item.price.original_price}</del></div>
+            <div class="discount_percentage_one">(%${item.price.discount} OFF)</div>
           </div>
           <button class="getProduct_one">Add TO Cart</button>
         </div>
 `
+})
+output_container.innerHTML=itemContainer
