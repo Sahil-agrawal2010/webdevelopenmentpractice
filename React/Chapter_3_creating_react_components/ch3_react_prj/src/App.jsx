2d2bd0ede4b49ed6aa24b9ffadcffb54826da866
sahil agrawal <agrawalsahil114@gmail.com>
2025-12-30 19:01:53 +0545

    done different exports today and learned its three types 3

--- React/Chapter_3_creating_react_components/ch3_react_prj/src/App.jsx
+++ React/Chapter_3_creating_react_components/ch3_react_prj/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import Main_button from "./ButtonComponenet"
+// function App(){
+//   return (<Main_button></Main_button>)
+// }
 
-function App() {
-  const [count, setCount] = useState(0)
+// export default App
 
+
+
+
+// import Secondary_button from "./ButtonComponenet";
+// function App(){
+//   return (<Secondary_button></Secondary_button>)
+// }
+
+// export default App
+
+
+
+
+// import { Third_button } from "./ButtonComponenet";
+// function App(){
+//   return (<Third_button></Third_button>)
+// }
+
+// export default App
+
+import { Main_button } from "./ButtonComponenet";
+import Secondary_button from "./ButtonComponenet";
+import { Third_button } from "./ButtonComponenet";
+function App(){
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Main_button></Main_button>
+      <Secondary_button></Secondary_button>
+      <Third_button></Third_button>
+    </div>
   )
 }
 
---
+++ React/Chapter_3_creating_react_components/ch3_react_prj/src/ButtonComponenet.jsx
@@ -0,0 +1,24 @@
+// function Main_button(){
+// return <button>Click Me</button>
+// }
+
+// export default Main_button
+
+
+
+
+
+export function Main_button(){
+    return <button>Click Me</button>
+}
+
+export default function Secondary_button(){
+    return <button>Click here also</button>
+} 
+
+
+
+
+export function Third_button(){
+    return <button>Click Here too</button>
+}
