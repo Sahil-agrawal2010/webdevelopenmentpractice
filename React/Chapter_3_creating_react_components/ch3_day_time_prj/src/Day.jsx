95a3b6c871e81838983ae4292165811915fdda18
sahil agrawal <agrawalsahil114@gmail.com>
2026-01-04 20:15:57 +0545

    done date and time task

--- React/Chapter_3_creating_react_components/ch3_day_time_prj/src/Day.jsx
+++ React/Chapter_3_creating_react_components/ch3_day_time_prj/src/Day.jsx
@@ -1,7 +1,5 @@
 export function CurrentDay(){
   const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-let day= new Date().getDays().toString()
-  return(
-    day[days]
-  )
+let day = new Date().getDay()
+  return days[day]
 }
