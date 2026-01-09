6a65dfdac83bc2c0bab3757aae68fe683b83f085
sahil agrawal <agrawalsahil114@gmail.com>
2026-01-09 18:30:47 +0545

    nothing doen much today

--- React/Chapter_4_fragnment_map/ch4_conditionalreandering_prj/src/App.jsx
+++ React/Chapter_4_fragnment_map/ch4_conditionalreandering_prj/src/App.jsx
@@ -32,14 +32,18 @@ export default function App() {
   // }
 
   // let answer =
-  //   vegetables_array.length === 0 ? (
+    // vegetables_array.length === 0 ? (
   //     <h2>Sorry there are no veegtables found</h2>
     // ) : null;
+
+
+
+    
   return (
     <>
       <h1 className="vegetable_list">Vegetables</h1>
        {/* {(vegetables_array.length===0 && <h2>Sorry there are no vegetables found</h2> )}  */}
-       <ErrorMessage {sahil}></ErrorMessage>
+       <ErrorMessage {vegetable}></ErrorMessage>
       <ul className="list-group">
         {vegetables_array.map((vegetable) => (
           <li key={vegetable} className="list-group-item">
