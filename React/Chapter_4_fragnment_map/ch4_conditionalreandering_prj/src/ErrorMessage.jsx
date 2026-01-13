22d19460d51bbd89d34d5c45d6479ff4c35866c8
sahil agrawal <agrawalsahil114@gmail.com>
2026-01-13 18:20:21 +0545

    done more on api fetching a bit left and api control is also left

--- React/Chapter_4_fragnment_map/ch4_conditionalreandering_prj/src/ErrorMessage.jsx
+++ React/Chapter_4_fragnment_map/ch4_conditionalreandering_prj/src/ErrorMessage.jsx
@@ -14,23 +14,87 @@ export default function ErrorMessage(props) {
       <li class="list-group-item">
         <p>Age : {props.studentsDetails.basic_info.personal.age}</p>
       </li>
-      <li class="list-group-item">Education Level : {props.studentsDetails.basic_info.education.level}</li>
-      <li class="list-group-item">Education faculty : {props.studentsDetails.basic_info.education.faculty}</li>
-      <li class="list-group-item">Name Of Collage : {props.studentsDetails.basic_info.education.college.name}</li>
-      <li class="list-group-item">Name Of University : {props.studentsDetails.basic_info.education.college.university}</li>
-      <li class="list-group-item">City : {props.studentsDetails.basic_info.education.college.location.city}</li>
-      <li class="list-group-item">Province : {props.studentsDetails.basic_info.education.college.location.province}</li>
-      <li class="list-group-item"> Address : {props.studentsDetails.address.history.stayed_places[0].city}</li>
-      <li class="list-group-item">Stayed From : {props.studentsDetails.address.history.stayed_places[0].duration.from}</li>
-      <li class="list-group-item">Stayed Till : {props.studentsDetails.address.history.stayed_places[0].duration.to}</li>
-      <li class="list-group-item">City : {props.studentsDetails.address.history.stayed_places[1].city}</li>
-      <li class="list-group-item">Stayed From : {props.studentsDetails.address.history.stayed_places[1].duration.from}</li>
-      <li class="list-group-item">Stayed to : {props.studentsDetails.address.history.stayed_places[1].duration.to}</li>
-      <li class="list-group-item">Current City : {props.studentsDetails.address.current.temporary.city}</li>
-      <li class="list-group-item">Current Province : {props.studentsDetails.address.current.temporary.district}</li>
-      <li class="list-group-item">Current Latitude : {props.studentsDetails.address.current.temporary.geo.lat}</li>
-      <li class="list-group-item">Current City : {props.studentsDetails.address.current.permanent.city}</li>
-      <li class="list-group-item">Current Province : {props.studentsDetails.address.current.permanent.province}</li>
+      <li class="list-group-item">
+        Education Level : {props.studentsDetails.basic_info.education.level}
+      </li>
+      <li class="list-group-item">
+        Education faculty : {props.studentsDetails.basic_info.education.faculty}
+      </li>
+      <li class="list-group-item">
+        Name Of Collage :{" "}
+        {props.studentsDetails.basic_info.education.college.name}
+      </li>
+      <li class="list-group-item">
+        Name Of University :{" "}
+        {props.studentsDetails.basic_info.education.college.university}
+      </li>
+      <li class="list-group-item">
+        City :{" "}
+        {props.studentsDetails.basic_info.education.college.location.city}
+      </li>
+      <li class="list-group-item">
+        Province :{" "}
+        {props.studentsDetails.basic_info.education.college.location.province}
+      </li>
+      <li class="list-group-item">
+        {" "}
+        Address : {props.studentsDetails.address.history.stayed_places[0].city}
+      </li>
+      <li class="list-group-item">
+        Stayed From :{" "}
+        {props.studentsDetails.address.history.stayed_places[0].duration.from}
+      </li>
+      <li class="list-group-item">
+        Stayed Till :{" "}
+        {props.studentsDetails.address.history.stayed_places[0].duration.to}
+      </li>
+      <li class="list-group-item">
+        City : {props.studentsDetails.address.history.stayed_places[1].city}
+      </li>
+      <li class="list-group-item">
+        Stayed From :{" "}
+        {props.studentsDetails.address.history.stayed_places[1].duration.from}
+      </li>
+      <li class="list-group-item">
+        Stayed to :{" "}
+        {props.studentsDetails.address.history.stayed_places[1].duration.to}
+      </li>
+      <li class="list-group-item">
+        Current City : {props.studentsDetails.address.current.temporary.city}
+      </li>
+      <li class="list-group-item">
+        Current Province :{" "}
+        {props.studentsDetails.address.current.temporary.district}
+      </li>
+      <li class="list-group-item">
+        Current Latitude :{" "}
+        {props.studentsDetails.address.current.temporary.geo.lat}
+      </li>
+      <li class="list-group-item">
+        Current City : {props.studentsDetails.address.current.permanent.city}
+      </li>
+      <li class="list-group-item">
+        Current Province :{" "}
+        {props.studentsDetails.address.current.permanent.province}
+      </li>
+
+      <li class="list-group-item">
+        Favurite Breakfast :{" "}
+        {props.studentsDetails.preferences.food.dishes.breakfast[0].name}
+      </li>
+      <li class="list-group-item">
+        Favurite Breakfast Type :{" "}
+        {props.studentsDetails.preferences.food.dishes.breakfast[1].type}
+      </li>
+      <li class="list-group-item">
+        Favourite Dinner :{" "}
+        {props.studentsDetails.preferences.food.dishes.dinner[0].name}
+      </li>
+      <li class="list-group-item">
+        Favourite Dinner Calories :{" "}
+        {props.studentsDetails.preferences.food.dishes.dinner[0].calories}
+      </li>
+      {/* <li class="list-group-item"></li>
       <li class="list-group-item"></li>
       <li class="list-group-item"></li>
       <li class="list-group-item"></li>
@@ -47,12 +111,7 @@ export default function ErrorMessage(props) {
       <li class="list-group-item"></li>
       <li class="list-group-item"></li>
       <li class="list-group-item"></li>
-      <li class="list-group-item"></li>
-      <li class="list-group-item"></li>
-      <li class="list-group-item"></li>
-      <li class="list-group-item"></li>
-      <li class="list-group-item"></li>
-      <li class="list-group-item"></li>
+      <li class="list-group-item"></li> */}
     </ul>
   );
 }
