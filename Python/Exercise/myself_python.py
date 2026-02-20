1cb081f83aec61ee42cd29e3022549a6492905bf
sahil agrawal <agrawalsahil114@gmail.com>
2026-02-20 20:29:29 +0545

    done a bit more today

---
+++ Python/Exercise/myself_python.py
@@ -0,0 +1,7 @@
+userInput=int(input("Enter your age please :- "))
+d=userInput*365
+m=userInput*12
+h=d*24
+mins=h*60
+s=mins*60
+print(f"You have lived {d} days {m} month {h} hour {mins} minutes {s} seconds.")
