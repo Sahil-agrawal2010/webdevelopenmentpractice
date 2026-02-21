c90ba8bd08fe00addc54bf136edf607166c26521
sahil agrawal <agrawalsahil114@gmail.com>
2026-02-21 20:37:27 +0545

    done a bit more today

--- Python/Exercise/myself_python.py
+++ Python/Exercise/myself_python.py
@@ -1,7 +1,16 @@
-userInput=int(input("Enter your age please :- "))
-d=userInput*365
-m=userInput*12
-h=d*24
-mins=h*60
-s=mins*60
-print(f"You have lived {d} days {m} month {h} hour {mins} minutes {s} seconds.")
+# userInput=int(input("Enter your age please :- "))
+# d=userInput*365
+# m=userInput*12
+# h=d*24
+# mins=h*60
+# s=mins*60
+# print(f"You have lived {d} days {m} month {h} hour {mins} minutes {s} seconds.")
+
+
+
+
+p=int(input("Enter the principle :-"))
+r=int(input("Enter the rate :-"))
+t=int(input("Enter the time :-"))
+s=(p*r*t)/100
+print(s)
