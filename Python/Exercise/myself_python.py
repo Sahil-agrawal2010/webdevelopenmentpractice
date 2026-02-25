ae169c6e9c8e80b36217e515ebcc069dcc55ad1a
sahil agrawal <agrawalsahil114@gmail.com>
2026-02-25 20:19:16 +0545

    tried to do pime number

--- Python/Exercise/myself_python.py
+++ Python/Exercise/myself_python.py
@@ -31,5 +31,26 @@
 
 
 
-userInput = input("Enter anything you like :- ")
-print(len(userInput))
+# userInput = input("Enter anything you like :- ")
+# print(len(userInput))
+
+
+
+
+
+
+
+
+userInput = int(input("Enter any number accordint to your want and your requirnment :- "))
+counter=0
+for i in range(1,userInput+1):
+    if userInput%i==0 :
+        counter=counter+1
+        if counter>2:
+            break
+    if counter==2 :
+        print(f"The number {userInput} is a prime number!")
+    elif counter!=2 : 
+        print(f"The number {userInput} is not a prime number!")
+    else :
+        print(f"Error!")
