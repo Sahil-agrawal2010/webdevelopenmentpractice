d7298c0d74f174d967216b3fbc55fc1d6fcc39c4
sahil agrawal <agrawalsahil114@gmail.com>
2026-02-26 19:33:43 +0545

    task given today ok

--- Python/Exercise/myself_python.py
+++ Python/Exercise/myself_python.py
@@ -46,11 +46,9 @@ counter=0
 for i in range(1,userInput+1):
     if userInput%i==0 :
         counter=counter+1
-        if counter>2:
-            break
-    if counter==2 :
-        print(f"The number {userInput} is a prime number!")
-    elif counter!=2 : 
-        print(f"The number {userInput} is not a prime number!")
-    else :
-        print(f"Error!")
+if counter==2 :
+    print(f"The number {userInput} is a prime number!")
+elif counter!=2 : 
+    print(f"The number {userInput} is not a prime number!")
+else :
+    print(f"Error!")
--- Python/eighteen_for_loop.py
+++ Python/eighteen_for_loop.py
@@ -13,7 +13,72 @@
 
 
 
-for i in range(10):
-    if i==6:
-        continue
-    print(f"{i}")
+# for i in range(10):
+#     if i==6:
+#         continue
+#     print(f"{i}")
+    
+    
+    
+    
+    
+    
+    
+# userInput=input("Enter any work you like to :-").upper()
+# for i in range(len(userInput)):
+#     if (userInput[i]=="A") or (userInput[i]=="E") or (userInput[i]=="I") or (userInput[i]=="O") or (userInput[i]=="U"):
+#         print(userInput[i])
+        
+        
+        
+        
+# userInput = int(input("Enter any number accordint to your want and your requirnment :- "))
+# counter=0
+# for i in range(1,userInput+1):
+#     if userInput%i==0 :
+#         counter=counter+1
+# if counter==2 :
+#     print(f"The number {userInput} is a prime number!")
+# elif counter!=2 : 
+#     print(f"The number {userInput} is not a prime number!")
+# else :
+#     print(f"Error!")     
+    
+    
+    
+# for i in range(1,101):
+#     counter=0
+#     for j in range(1,i+1):
+#         if(i%j==0):
+#             counter+=1
+#     if(counter==2):
+#         print(i)
+        
+        
+        
+# hash="*"        
+# for i in range(1,11,2):
+#     print(hash)
+#     hash=hash+"**"
+    
+    
+    
+    
+    
+    
+    
+# hash = "*"
+# for i in range(5):
+#     for j in range(1,i+1):
+#         hash+="**"
+#     print(hash)
+
+
+
+
+hash = "*"
+for i in range(5):
+    print(hash)
+    hash+="**"
+    if(i==2):
+        hash="***"
