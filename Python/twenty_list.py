71a840069fbf59c599f774605fcd63aa1aeb7cd1
sahil agrawal <agrawalsahil114@gmail.com>
2026-03-02 22:52:01 +0545

    banking the money task doen

--- Python/twenty_list.py
+++ Python/twenty_list.py
@@ -49,22 +49,74 @@ fruits=["Apple","Banana","Carrot","Dragon fruit","Mango","Strauberry","Avacado",
 
 
 
-userInput=input("Enter the name of fruits [Q/q to quit] :- ")
-user_fruits=[]
-while (userInput!='q'):
-    user_fruits.append(userInput)
-    print(user_fruits)
-    user_delete_ask=input("Do you want to delete anything? (Yes/No) :- ").upper()
-    if(user_delete_ask=="YES"):
-        userAsk=input("Enter the name of fruit which you want to delete :-")
-        while userAsk not in user_fruits:
-            print("Pleasse enter the fruit which is alredy added in the array!")
-            print(user_fruits)
-            userAsk=input("Enter the name of fruit which you want to delete :-")
-        user_fruits.remove(userAsk)
-        print(user_fruits)
-        userInput=input("Enter the name of fruits [Q/q to quit] :- ")
-    else:
-        userInput=input("Enter the name of fruits [Q/q to quit] :- ")
-if userInput=="Q" or userInput=="q":
-    print("You quitted")
+# userInput=input("Enter the name of fruits [Q/q to quit] :- ")
+# user_fruits=[]
+# while (userInput!='q'):
+#     user_fruits.append(userInput)
+#     print(user_fruits)
+#     user_delete_ask=input("Do you want to delete anything? (Yes/No) :- ").upper()
+#     if(user_delete_ask=="YES"):
+#         userAsk=input("Enter the name of fruit which you want to delete :-")
+#         while userAsk not in user_fruits:
+#             print("Pleasse enter the fruit which is alredy added in the array!")
+#             print(user_fruits)
+#             userAsk=input("Enter the name of fruit which you want to delete :-")
+#         user_fruits.remove(userAsk)
+#         print(user_fruits)
+#         userInput=input("Enter the name of fruits [Q/q to quit] :- ")
+#     else:
+#         userInput=input("Enter the name of fruits [Q/q to quit] :- ")
+# if userInput=="Q" or userInput=="q":
+#     print("You quitted")
+    
+    
+    
+    
+# userAsk=int(input("Enter how many priority you want :- "))
+# for i in range(userAsk):
+#      userInput=input("Enter the to do list of priority [Hobby - Rank] :- ")
+#      numb=userInput[-1]
+#      if(numb>numb):
+     
+     
+     
+     
+     
+     
+     
+
+check=True
+account=[]
+total=0
+account_withdraw=0
+account_deposite=0
+userInput=input("What do you want to do withdraw or deposite the money :- ").upper()
+while(check):
+    if userInput=="WITHDRAW":
+        user_withdraw=int(input("Enter how many money do you want to withdraw :- "))
+        account.append(-user_withdraw)
+        for i in range(len(account)):
+            total=total+int(account[i])
+        if(total<0):
+            print("Sorry ! Your account balance is already zero ( 0 ) so please add money first and only withdraw it or take the loan on your owen expense and risk ! ")
+        print(account)
+    if userInput=="DEPOSITE":
+        user_deposite=int(input("Enter how many money do you want to deposite :- "))
+        account.append(user_deposite)
+        print(account)
+    userInput=input("What do you want to do withdraw or deposite the money or press Q/q to quit:- ").upper()
+    if(userInput=="Q" or userInput=="q"):
+        print("Quitted!")
+        check=False
+        for i in range(len(account)):
+            total=total+int(account[i])
+        # if(total<0):
+        #     print("Sorry ! Your account balance is already zero ( 0 ) so please add money first and only withdraw it or take the loan on your owen expense and risk ! ")
+        #     check=False
+            if account[i]>=0:
+                account_deposite=account_deposite+account[i]
+            else:
+                account_withdraw=account_withdraw+account[i]
+        print(f"Total account balance :- {total}")
+        print(f"Total account withdraw :- {account_withdraw}")
+        print(f"Total account deposite :- {account_deposite}")
