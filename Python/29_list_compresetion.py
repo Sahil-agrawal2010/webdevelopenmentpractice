a06147277bcaa28898fd220f34921e9d6d3656b2
sahil agrawal <agrawalsahil114@gmail.com>
2026-03-11 20:19:43 +0545

    all the task done

--- Python/29_list_compresetion.py
+++ Python/29_list_compresetion.py
@@ -24,15 +24,61 @@
 
 
 
-numb=[10,-3,4,-6,7,-2]
-positive_zero=[i if i>=0 else 0 for i in numb]
-print(positive_zero)
+# numb=[10,-3,4,-6,7,-2]
+# positive_zero=[i if i>=0 else 0 for i in numb]
+# print(positive_zero)
 
 
 
 
 
 # Remove empty srtings
+
+
+
+
+empty=["Apple","","Ball","","Call","","Doctor"]
+remove_empty=[i if len(i)>0 else None for i in empty]
+print(f"{remove_empty}")
+
+
+
+
+
 # First letter of each word
+
+
+
+
+words=["Avacado","Ballon","Cat","Dark"]
+first_letter=[i[0] for i in words]
+print(first_letter)
+
+
+
+
+
 # Celcious to farahnite
+
+
+
+
+celcius=[100,37,80]
+frahnite=[(i*(9/5)+32) for i in celcius]
+print(frahnite)
+
+
+
+
+
 # Square in tuple [(1,1)]
+
+
+
+
+
+squares=[((i),(i*i)) for i in range(1,6)]
+print(squares)
+
+
+
