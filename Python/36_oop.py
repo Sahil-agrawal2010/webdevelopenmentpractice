a6d1d0e3bc7c45ab41f330e2464b32cf1dfa8968
sahil agrawal <agrawalsahil114@gmail.com>
2026-03-18 19:24:46 +0545

    doena bit more on callss and object and oop

---
+++ Python/36_oop.py
@@ -0,0 +1,12 @@
+# There are two types of proggramming language :- 
+# 1) Procedure oriented
+# 2) Object oriented
+# POP Procedureal Oriented Programming :- It follows top to bottom apporach , runs sequentially , uses functions mainly 
+# OOP Object Oriented Programming :- It follows bottom up apporoach , uses real world entities call as objects , class .
+# Class :- It is a blue print of real world
+# Objects :- Instense of class , has it's own method
+# There are four pillars in OOP :- 
+# 1) Encapsulation
+# 2) Polymerpism
+# 3) Inheritense
+# 4) Abstraction
---
+++ Python/37_class_and_objects.py
@@ -0,0 +1,36 @@
+# class Sahil:
+#     print("Helloo World!")
+#     name="Sahil"
+#     age=15
+#     is_Married=False
+    
+    
+    
+    
+#     def info(self):
+#         print(f"My name is {self.name}.My age is {self.age}.My marriatial status is {self.is_Married}")
+    
+    
+    
+    
+# obj1=Sahil()
+# print(obj1)
+# print(obj1.name)
+# print(obj1.age)
+# print(obj1.is_Married)
+# obj1.info()
+
+
+
+
+
+class shape:
+    def area(self,a,b):
+        return a*b
+    
+    
+    
+    
+    
+obj1=shape()
+print(f"The area is {obj1.area(5,3)}")
