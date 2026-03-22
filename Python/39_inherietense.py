35a8a9e78781828e9097e913e3a91f1b730babc6
sahil agrawal <agrawalsahil114@gmail.com>
2026-03-22 20:23:17 +0545

    done a bit more

--- Python/39_inherietense.py
+++ Python/39_inherietense.py
@@ -33,45 +33,72 @@
 
 
 
+# class Parent():
+#     def parent_method(self):
+#         print("This is a parent method")
+        
+        
+        
+# class Child(Parent):
+#     def child_method(self):
+#         print("This is a child method")
+        
+        
+# class Children(Child):
+#     def children_method(self):
+#         print("This is a children mehod")
+# obj=Children()
+# obj.children_method()
+# obj.parent_method()
+# obj.child_method()
+
+
+
+
+# # Multiple :- 
+# class A():
+#     def a_mehod(self):
+#         print("I am from A")
+# class B():
+#     def b_mehod(self):
+#         print("I am from B")
+# class C():
+#     def c_mehod(self):
+#         print("I am from C")
+# class D(A,B,C):
+#     def d_mehod(self):
+#         print("I am from D")
+        
+        
+# obj=D()
+# obj.a_mehod()
+# obj.b_mehod()
+# obj.c_mehod()
+# obj.d_mehod()
+
+
+
+
+
+
 class Parent():
-    def parent_method(self):
-        print("This is a parent method")
+    def __init__(self,name):
+        self.my_name=name
+        
+        
+        
+    def displayy(self):
+        print(f"My name is {self.my_name}")
         
         
         
 class Child(Parent):
-    def child_method(self):
-        print("This is a child method")
+    name="Sahil"
+    def display(self):
+        print(f"My name is {self.name}")
         
         
-class Children(Child):
-    def children_method(self):
-        print("This is a children mehod")
-obj=Children()
-obj.children_method()
-obj.parent_method()
-obj.child_method()
-
-
-
-
-# Multiple :- 
-class A():
-    def a_mehod(self):
-        print("I am from A")
-class B():
-    def b_mehod(self):
-        print("I am from B")
-class C():
-    def c_mehod(self):
-        print("I am from C")
-class D(A,B,C):
-    def d_mehod(self):
-        print("I am from D")
         
-        
-obj=D()
-obj.a_mehod()
-obj.b_mehod()
-obj.c_mehod()
-obj.d_mehod()
+obj=Child("Agrawal")
+obj.display()
+obj.displayy()
